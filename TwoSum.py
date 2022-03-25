aef56eefdf23533ed0ab390bf2a0b34dbdc6873d
Henry Meier <32826270+hcm444@users.noreply.github.com>
2022-03-25 01:21:26 -0700

    Update TwoSum.py
    3/25/22

--- TwoSum.py
+++ TwoSum.py
@@ -7,8 +7,13 @@ class Solution(object):
         """
         counter = 0
         for i in range(0,len(nums)):
+            #search for one index
             for j in range(0,len(nums)):
+                #search for second
                 if (i !=j):
+                    #can't be equal or you might get[0,0]
                     if nums[i] + nums[j] == target:
+                        #x + y = target
                         return [int(i),int(j)]
+                        #return
                         counter +=1
